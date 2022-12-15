33f879c07fd145ce87fe5347d36d85a387bf770c
Gabriel Fidelis Pereira <jpinformaticafoz@gmail.com>
2022-12-15 17:47:21 -0300

    'fixes'

--- vuetify_vuex/src/components/ConfigModal.vue
+++ vuetify_vuex/src/components/ConfigModal.vue
@@ -21,36 +21,195 @@
                     <v-icon aria-hidden="false" dark color="grey lighten-2">mdi-cog</v-icon>
                 </v-btn>
             </template>
-            <v-card dark>
-                <v-sheet color="grey darken-3">
-                    <v-card-actions class="d-flex justify-start ml-n2 mt-n1">
-                        <v-btn @click="(dialog = false)" icon color="red accent-2">
-                            <v-icon color="red accent-2">mdi-close</v-icon>
-                        </v-btn>
-                    </v-card-actions>
-                    <v-card-title class="mt-n6">
-                        <p>Configurações Gerais do Sistema</p>
-                    </v-card-title>
-                    <v-card-subtitle class="mt-n5">
-                        <p class="ml-2">Tudo que for alterado aqui, afetará diretamente o sistema...</p>
-                    </v-card-subtitle>
+            <v-card dark class="cardi">
+                <v-sheet color="grey darken-3" :elevation="4">
+                            <v-card-title >
+                                <p>Configurações Gerais do Sistema</p>
+                            </v-card-title>
+                            <v-card-subtitle class="mt-n8">
+                                <p class="ml-2">Tudo que for alterado aqui, afetará diretamente o sistema...</p>
+                            </v-card-subtitle>
                 </v-sheet>
-            </v-card>
+                <v-card-text class="mt-2">
+                    <v-row>
+                        <v-col cols="4">
+                            <small>Somente o dia é considerado nas datas !</small>
+                            <v-switch
+                                v-model="adiantamento"
+                                label="Utilizar Adiantamento..."
+                                color="teal accent-3"
+                                hide-details
+                            ></v-switch>
+                        </v-col>
+                       
+                        <v-divider color="white" class="mt-2" vertical></v-divider>
+                        <v-col cols="4" >
+                            <small>Utiliza para atribuir comissão aos vendedores</small>
+                            <v-switch
+                                v-model="comissao"
+                                label="Ativar comissões..."
+                            
+                                color="teal accent-3"
+                                hide-details
+                            ></v-switch>
+                        </v-col>
+                        <v-divider color="white" class="mt-2" vertical></v-divider>
+                        <v-col cols="4" >
+                            <small>Utilizar Tabela Virtual para relatorios</small>
+                            <v-switch
+                                v-model="tabelaVirtual"
+                                label="Ativar Tabela Virtual no Relátorio"
+                            
+                                color="teal accent-3"
+                                hide-details
+                            ></v-switch>
+                        </v-col>
+                    </v-row>
+                    <v-row >
+                        <v-col cols="4" >
+                            <v-text-field
+                                v-model="dtPagamentoSalario"
+                                label="Data de Pagamento do Salário"
+                                persistent-hint
+                                required
+                                color="teal lighten-1"
+                                type="date"
+                                dark
+                            ></v-text-field>       
+                        </v-col>
+                        <v-divider vertical color="white" ></v-divider>
+                        <v-col cols="4" >  
+                            <v-text-field
+                                v-if="comissao" 
+                                v-model="porcentagemComissao"
+                                label="Porcentagem da Comissão"
+                                persistent-hint
+                                required
+                                placeholder="Valores entre 0.01 a 99.99"
+                                max="99.99"
+                                color="teal lighten-1"
+                                type="number"
+                                min="0.01"    
+                            ></v-text-field>
+                        </v-col>
+                        <v-divider vertical color="white" ></v-divider>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="4" v-if="adiantamento">
+                            <v-text-field
+                                v-model="dtPagamentoAdiantamento"
+                                label="Data de Pagamento do Adiantamento"
+                                persistent-hint
+                                required
+                                color="teal lighten-1"
+                                type="date"
+                                dark
+                            ></v-text-field>
+                        </v-col>
+                        <v-divider v-if="adiantamento" vertical color="white" ></v-divider>
+                        <v-col cols="4">
 
+                        </v-col>
+                        <v-divider v-if="adiantamento" vertical color="white" ></v-divider>
+                    </v-row>
+                </v-card-text>
+            </v-card>
+            <v-footer  fixed color="transparent">
+                <v-row>
+                    <v-col class="d-flex justify-end" >
+                        <v-btn icon fab>
+                            <v-icon x-large color="red darken-2">
+                                mdi-arrow-u-left-top-bold
+                             </v-icon>
+                        </v-btn>
+                        <v-btn icon fab @click="test">
+                            <v-icon x-large color="teal darken-2">
+                               mdi-content-save
+                             </v-icon>
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-footer>
         </v-dialog>
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     data(){
         return {
-            dialog : false
+            dialog : false,
+            adiantamento : false,
+            dtPagamentoSalario : '',
+            dtPagamentoAdiantamento : '',
+            comissao : false, 
+            porcentagemComissao : 0.01,
+            tabelaVirtual : false,
         }
-    }
+    },
+    methods:{
+        ...mapActions('userMod', ['setAjustesFolha', 'showAjusteFolha']),
+        async openAjustes(){
+            let obj = await this.showAjusteFolha()
+            if(obj.DT_ADIANTAMENTO !== null && obj.DT_ADIANTAMENTO !== undefined){
+                this.adiantamento = true
+                this.dtPagamentoAdiantamento = this.formatDatas(obj.DT_ADIANTAMENTO) 
+                console.log(this.dtPagamentoAdiantamento)
+            }
+            this.dtPagamentoSalario = this.formatDatas(obj.DT_SALARIO)
+        },
+        async setAjustes(){
+            let compararData = this.comparaDatas(this.dtPagamentoSalario, this.dtPagamentoAdiantamento)
+            if(!compararData){
+                let payload = {DT_PAGAMENTO : this.dtPagamentoSalario, 
+                    DT_ADIANTAMENTO : this.dtPagamentoAdiantamento, FLAG : this.adiantamento}
+                this.msg = await this.setAjustesFolha(payload)
+                this.registro = true
+                this.active = false
+            }
+        },
+        formatDatas(obj){
+            obj = new Date(obj).toLocaleDateString('en-CA');
+            return obj
+        },
+        test(){
+            this.porcentagemComissao = parseFloat(this.porcentagemComissao)
+            console.log(this.porcentagemComissao)
+        }
+
+    },
+    watch: {
+        adiantamento: function (val) {
+            if (val == false) {
+                this.dtPagamentoAdiantamento = '';
+            }
+            this.adiantamento = val;
+        },
+        porcentagemComissao : function(val){
+            if(val <= 0 || val == '' || val == null){
+                this.porcentagemComissao = 0.01
+            }
+            if(val >= 99.99){
+                this.porcentagemComissao = 99.99
+            }
+        }
+    },
+   
+    async created() {
+        await this.openAjustes()
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+#linha-vertical {
+    height: 200px;
+    border-right: 1px solid red;
+}
+.footer{
+    height: 100%; 
+}
 
 </style>
